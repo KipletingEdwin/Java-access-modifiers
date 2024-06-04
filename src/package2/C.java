2a3e1c65bc66bf93f9ad366b7b1d643ce8086004
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-04 15:40:26 +0300

    finalize the code

---
+++ src/package2/C.java
@@ -0,0 +1,12 @@
+package package2;
+import  package1.*;
+
+public class C {
+
+    String defaultMessage = "This is the default";
+
+    public  String publicMessage = "This is public";
+    private  String privateMessage = " This is private";
+
+
+}
